45f5df073902de52a20e839a8f6025f0524c9a15
vanfranrocha <vanfranrocha@hotmail.com>
2016-10-05 00:33:23 -0300

    add layout

---
+++ Ferramenta_Scrumt/Controllers/SprintsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ferramenta_Scrumt.Controllers
+{
+    public class SprintsController : Controller
+    {
+        // GET: Sprints
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListSprints()
+        {
+            return View();
+        }
+    }
+}
