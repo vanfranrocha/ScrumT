4c8224c7c98f9472ebb0db0ef3045c04db34bf46
vanfranrocha <vanfranrocha@hotmail.com>
2016-11-20 20:25:36 -0200

    add mapper and repository releasebacklog

---
+++ Ferramenta_Scrumt/REPOSITORIO/ReleaseBacklogBacklogMapper.cs
@@ -0,0 +1,21 @@
+﻿using Ferramenta_Scrumt.MODEL;
+using System.Data;
+
+namespace Ferramenta_Scrumt.REPOSITORIO
+{
+    public class ReleaseBacklogMapper : SqlMapperBase<ReleaseBacklog>
+    {
+        public override ReleaseBacklog MapFromSource(DataRow Record)
+        {
+            ReleaseBacklog relback = new ReleaseBacklog();
+
+            relback.ID = (int)Record["ID_Projeto"];
+            relback.ID_Membro = (int)Record["ID_Membro"];
+            relback.ID_Pbacklog = (int)Record["ID_PBacklog"];
+            relback.Nome_Membro = (string)Record["Nome"];
+            relback.Situacao_Quadro = (string)Record["Situacao_QuadroTarefas"];
+            relback.Historia = (string)Record["Historia"];
+            return relback;
+        }
+    }
+}
---
+++ Ferramenta_Scrumt/REPOSITORIO/ReleaseBacklogRepositorio.cs
@@ -0,0 +1,71 @@
+﻿using Ferramenta_Scrumt.INFRA;
+using Ferramenta_Scrumt.MODEL;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ferramenta_Scrumt.REPOSITORIO
+{
+    public class ReleaseBacklogRepositorio : ISQLRepository<ReleaseBacklog>
+    {
+        DBUtil DB = new DBUtil();
+
+        public ReleaseBacklog FindByID(int ID, ISQLMapper<ReleaseBacklog> mapper)
+        {
+            return mapper.MapFromSource(DB.GetByID("SP_RBACKLOG_BYID", ID));
+        }
+
+        public List<ReleaseBacklog> Lista(ISQLMapper<ReleaseBacklog> mapper)
+        {
+            SqlParameter[] Param = new SqlParameter[]
+            { };
+            string SQL = "Select [ID_ProductRelease],[Product_Backlog].[ID_PBacklog], [Product_Backlog].Historia,[Users].[Nome],[ID_Membro],[Estimativa_Inicio],[Situacao_QuadroTarefas] from Product_Release Inner Join Users on Product_Release.ID_Membro = Users.ID_Equipe Inner Join Product_Backlog on Product_Release.ID_PBacklog = Product_Backlog.ID_PBacklog";
+            return mapper.MapAllFromSource(DB.ListaSQL(Param, SQL).Tables[0]);
+        }
+      
+        public void ADD(ReleaseBacklog Item)
+        {
+            SqlParameter ID = new SqlParameter("@ID_ProductRelease", SqlDbType.Int);
+            ID.Direction = ParameterDirection.Output;
+
+            SqlParameter[] Param = new SqlParameter[]
+            {
+                new SqlParameter("@ID_Membro",Item.ID_Membro),
+                new SqlParameter("@ID_PBacklog",Item.ID_Pbacklog),
+                new SqlParameter("@Situacao_Quadrotarefas",Item.Situacao_Quadro),
+                new SqlParameter("@Estimativa_Inicio",Item.Estimativa_inicio),
+                ID
+            };
+
+            DB.ExecSP("SP_RBACKLOG_INCLUIR", Param);
+        }
+
+        public void Update(ReleaseBacklog Item)
+        {
+
+            SqlParameter[] Param = new SqlParameter[]
+            {
+                new SqlParameter("@ID_Membro",Item.ID_Membro),
+                new SqlParameter("@ID_PBacklog",Item.ID_Pbacklog),
+                new SqlParameter("@Situacao_Quadrotarefas",Item.Situacao_Quadro),
+                new SqlParameter("@Estimativa_Inicio",Item.Estimativa_inicio),
+                new SqlParameter("@ID",Item.ID)
+            };
+
+            DB.ExecSP("SP_PROJETO_UPDATE", Param);
+        }
+
+        public void Delete(ReleaseBacklog Item)
+        {
+            SqlParameter[] Param = new SqlParameter[]
+            {
+                new SqlParameter("@ID",Item.ID)
+            };
+
+            DB.ExecSP("SP_PROJETO_DELETE", Param);
+
+        }
+
+
+    }
+}
