9da3d4559113ff13c95e6337520793e96f9dec29
vanfranrocha <vanfranrocha@hotmail.com>
2016-11-20 23:31:50 -0200

    add controler and view index quadro de tarefas

---
+++ Ferramenta_Scrumt/Controllers/QuadrotarefasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ferramenta_Scrumt.Controllers
+{
+    [Authorize]
+    public class QuadrotarefasController : Controller
+    {
+        // GET: Quadrotarefas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
