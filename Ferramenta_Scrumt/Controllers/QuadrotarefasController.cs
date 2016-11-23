034a649ef39f1759abf51418e2d33126a283e7b6
vanfranrocha <vanfranrocha@hotmail.com>
2016-11-22 23:23:16 -0200

    improve notification layout

--- Ferramenta_Scrumt/Controllers/QuadrotarefasController.cs
+++ Ferramenta_Scrumt/Controllers/QuadrotarefasController.cs
@@ -18,7 +18,7 @@ namespace Ferramenta_Scrumt.Controllers
 
         private void CarregaLista()
         {
-            QtarefasList = _QtarefasRep.Listaquadro(new QuadrotarefasMapper(), "Select [Product_Backlog].[Historia],[Projeto].[Descricao],[Product_Release].[ID_PBacklog],[Situacao_Quadrotarefas] from Product_Release Inner Join Product_Backlog on Product_Release.ID_PBacklog = Product_Backlog.ID_PBacklog Inner Join Projeto on Product_Backlog.ID_Projeto = Projeto.ID_Projeto");
+            QtarefasList = _QtarefasRep.Listaquadro(new QuadrotarefasMapper(), "Select [Product_Backlog].[Historia],[Projeto].[Descricao],[Product_Release].[ID_PBacklog],[Situacao_Quadrotarefas] from Product_Release Inner Join Product_Backlog on Product_Release.ID_PBacklog = Product_Backlog.ID_PBacklog Inner Join Projeto on Product_Backlog.ID_Projeto = Projeto.ID_Projeto group by Historia,Descricao, Product_Backlog.ID_PBacklog, Product_Release.ID_PBacklog, Situacao_QuadroTarefas");
             Session["Lista"] = QtarefasList;
             TestList = _TestRep.Listatest(new TesteUnidadeMapper());
             ViewBag.tes = new MultiSelectList(TestList, "Status", "Historia", "Classe");
