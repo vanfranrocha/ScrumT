fd58fef153d439d5363ebd0650d327f73a90cef5
vanfranrocha <vanfranrocha@hotmail.com>
2017-09-29 22:57:17 -0300

    improve js

--- Ferramenta_Scrumt/Scripts/jquery-3.1.1.js
+++ Ferramenta_Scrumt/Scripts/jquery-3.1.1.js
@@ -3837,8 +3837,8 @@ jQuery.extend( {
 // These usually indicate a programmer mistake during development,
 // warn about them ASAP rather than swallowing them by default.
 var rerrorNames = /^(Eval|Internal|Range|Reference|Syntax|Type|URI)Error$/;
-/*
-jQuery.Deferred.exceptionHook = function( error, stack ) {
+
+/*jQuery.Deferred.exceptionHook = function( error, stack ) {
 
 	// Support: IE 8 - 9 only
 	// Console exists when dev tools are open, which can happen at any time
